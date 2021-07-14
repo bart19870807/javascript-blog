d61d82dfc0a17a0d3e73c11ba9d437aa054ffc43
Bartlomiej Stachowski <bart.stachowski@gmail.com>
2021-07-14 11:45:35 +0100

    start modul 7

--- js/script.js
+++ js/script.js
@@ -44,7 +44,8 @@ const titleClickHandler = function(event){
 
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
-  optTitleListSelector = '.titles';
+  optTitleListSelector = '.titles',
+  optArticleTagsSelector = '.post-tags .list';
 
     
 
@@ -91,7 +92,40 @@ function generateTitleLinks() {
     link.addEventListener('click', titleClickHandler);
   }
 }
-
-
-
 generateTitleLinks();
+
+function generateTags() {
+/* find all articles */
+  const articles = document.querySelectorAll('.post');
+  /* START LOOP: for every article: */
+  for(let article of articles) {
+  /* find tags wrapper */
+    const optWrapTags = article.querySelector(optArticleTagsSelector);
+    console.log(optWrapTags);
+    /* make html variable with empty string */
+    let html = '';
+
+
+  }
+  
+
+ 
+
+  /* get tags from data-tags attribute */
+
+  /* split tags into array */
+
+  /* START LOOP: for each tag */
+
+  /* generate HTML of the link */
+
+  /* add generated code to html variable */
+
+  /* END LOOP: for each tag */
+
+  /* insert HTML of all the links into the tags wrapper */
+
+  /* END LOOP: for every article: */
+}
+
+generateTags();
