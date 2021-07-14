cf8c05f824f9378ff57bd764fd5f1713a65f83e1
Bartlomiej Stachowski <bart.stachowski@gmail.com>
2021-07-14 07:44:09 +0100

    Add new files

--- js/script.js
+++ js/script.js
@@ -4,92 +4,92 @@
 //     console.log('links:', links);
 // });
 const titleClickHandler = function(event){
-    event.preventDefault();
-    const clickedElement = this;
+  event.preventDefault();
+  const clickedElement = this;
     
-    console.log('Link was clicked!');
-    // console.log(event);
-    /* [DONE] remove class 'active' from all article links  */
+  console.log('Link was clicked!');
+  // console.log(event);
+  /* [DONE] remove class 'active' from all article links  */
 
-    const activeLinks = document.querySelectorAll('.titles a.active');
-    // stała w której wybierane są wszystkie artykuły które są aktywne
-    for(let activeLink of activeLinks) {
-        activeLink.classList.remove('active');
-    }
-    // tu zachodzi wyszukanie wszystkich linków z klasą active i po znalezieniu
-    // usuniecie - remove klasy active
+  const activeLinks = document.querySelectorAll('.titles a.active');
+  // stała w której wybierane są wszystkie artykuły które są aktywne
+  for(let activeLink of activeLinks) {
+    activeLink.classList.remove('active');
+  }
+  // tu zachodzi wyszukanie wszystkich linków z klasą active i po znalezieniu
+  // usuniecie - remove klasy active
 
-    /* [IN PROGRESS] add class 'active' to the clicked link */
-    console.log('clickedElement:', clickedElement);
-    console.log('clickedElement (with plus):'+clickedElement);
-    clickedElement.classList.add('active');
+  /* [IN PROGRESS] add class 'active' to the clicked link */
+  console.log('clickedElement:', clickedElement);
+  console.log('clickedElement (with plus):'+clickedElement);
+  clickedElement.classList.add('active');
 
-    /* [DONE] remove class 'active' from all articles */
-    const activeArticles = document.querySelectorAll('.post');
-    for(let activeArticle of activeArticles){
-        activeArticle.classList.remove('active')
-    }
+  /* [DONE] remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.post');
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
 
-    /* get 'href' attribute from the clicked link */
-    const articleSelector = this.getAttribute('href');
-    console.log(articleSelector);
+  /* get 'href' attribute from the clicked link */
+  const articleSelector = this.getAttribute('href');
+  console.log(articleSelector);
 
-    /* find the correct article using the selector (value of 'href' attribute) */
-    const targetArticle = document.querySelector(articleSelector);
-    console.log(targetArticle);
-    /* add class 'active' to the correct article */
-    targetArticle.classList.add('active');
-}
+  /* find the correct article using the selector (value of 'href' attribute) */
+  const targetArticle = document.querySelector(articleSelector);
+  console.log(targetArticle);
+  /* add class 'active' to the correct article */
+  targetArticle.classList.add('active');
+};
 // funkcja która wypisuje nam w konsoli informację że link został klikniety
 
 const optArticleSelector = '.post',
-    optTitleSelector = '.post-title',
-    optTitleListSelector = '.titles';
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
 
     
 
-    function generateTitleLinks() {
+function generateTitleLinks() {
          
-        /* remove contents of titleList */
-        const titleList = document.querySelector(optTitleListSelector);
-        titleList.innerHTML = "";
-        console.log(titleList);
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+  console.log(titleList);
 
-        /* for each article */
-        const articles = document.querySelectorAll('.post');
+  /* for each article */
+  const articles = document.querySelectorAll('.post');
         
-        let html = "";
-        for(let article of articles){
+  let html = '';
+  for(let article of articles){
 
-        /* get the article id */
-        const articleId = article.getAttribute('id');
-        console.log(articleId);
+    /* get the article id */
+    const articleId = article.getAttribute('id');
+    console.log(articleId);
 
-        /* find the title element */
-        const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    /* find the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
 
-        /* get the title from the title element */
+    /* get the title from the title element */
         
-        /* create HTML of the link */
-        const linkHTML ='<li><a href="#' + articleId +' "><span>' + articleTitle + '</span></a></li>';
-        console.log(linkHTML);
-        /* insert link into titleList */
+    /* create HTML of the link */
+    const linkHTML ='<li><a href="#' + articleId +' "><span>' + articleTitle + '</span></a></li>';
+    console.log(linkHTML);
+    /* insert link into titleList */
         
-        // petla for w ktorej dokonuje się iteracja links która przypisana jest do zmiennej link
-        // po wybraniu i kliknięci linku wykonywana jest funkcja która wyświetla komunikat
-        html = html + linkHTML;
+    // petla for w ktorej dokonuje się iteracja links która przypisana jest do zmiennej link
+    // po wybraniu i kliknięci linku wykonywana jest funkcja która wyświetla komunikat
+    html = html + linkHTML;
         
                     
-    }
+  }
     
-    console.log(html);
-    titleList.innerHTML = html;
-    const links = document.querySelectorAll('.titles a');
-        console.log(links);
-        // stała links w której wybieramy wszystkie selectory title a
-        for(let link of links){
-        link.addEventListener('click', titleClickHandler);
-        }
+  console.log(html);
+  titleList.innerHTML = html;
+  const links = document.querySelectorAll('.titles a');
+  console.log(links);
+  // stała links w której wybieramy wszystkie selectory title a
+  for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+  }
 }
 
 
