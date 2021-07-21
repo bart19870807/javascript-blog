87de982b936db26709a6487073c39fbd60e477f8
Bartlomiej Stachowski <bart.stachowski@gmail.com>
2021-07-21 12:37:24 +0100

    Update

--- js/script.js
+++ js/script.js
@@ -192,7 +192,7 @@ function generateTags() {
     /* [NEW] START LOOP: for each tag in allTags: */
     for(let tag in allTags){
       /* [NEW] generate code of a link and add it to allTagsHTML */
-      const tagLinkHTML = '<li><a href="#tag-'+ tag +'">'+ tag + calculateTagClass(allTags[tag], tagsParams) + '</a></li>';
+      const tagLinkHTML = '<li><a class="' + calculateTagClass(allTags[tag], tagsParams) + '" href="#tag-'+ tag +'">'+ tag +  + '</a></li>';
       console.log('tagLinkHTML', tagLinkHTML);
       allTagsHTML += tagLinkHTML;
     }
